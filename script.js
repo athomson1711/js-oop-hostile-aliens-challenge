a2c59d3fc4b0df415277b7b9fd916dc70a503db8
Andrew Thomson <athomson1711@gmail.com>
2022-06-26 18:58:47 +0100

    css grid area working

--- script.js
+++ script.js
@@ -1,57 +1,54 @@
+const fireButton = document.querySelector(".fire-button")
+const newGameButton = document.querySelector(".new-game-button")
+const damageAlertBox = document.querySelector(".damage-alert-box")
 
 
 class Ship {
-    constructor(shipName, hitPoints, damagePoints, className) {
+    constructor(shipName, hitPoints, damagePerHit,className) {
         this.shipName = shipName;
         this.hitPoints = hitPoints;
-        this.damagePoints = damagePoints;
-        // this.className = className;
-        // this.hasBeenDestroyed = false;
-    }
+        this.damagePerHit = damagePerHit;
+        this.className = className;
+    };
 
-    attackShip() {
-        let damageTaken = this.hitPoints -= this.damagePoints;
-        this.hitPoints = damageTaken
-        // if(this.hitPoints <= 0 ){
-        //     this.hasBeenDestroyed = true;
-        //     console.log(`${this.shipName} has taken ${this.damagePoints} damage and has been destroyed`)
-        //     console.log(`hasBeenDestroyed = ${this.hasBeenDestroyed}`)
-        // } else{
-        //     console.log(`${this.shipName} has taken ${this.damagePoints} damage and its remaning health is ${this.hitPoints} points`)
-        //     console.log(`hasBeenDestroyed = ${this.hasBeenDestroyed}`)
-        // } 
-    }
+        damageShip() {
+            this.hitPoints = this.hitPoints - this.damagePerHit;
+            damageAlertBox.innerHTML = "";
+            damageAlertBox.innerHTML = `${this.shipName} was dealt ${this.damagePerHit} damage`
 
-}
-
-class MotherShip extends Ship {
-    constructor() {
-        super("Mothership", 100, 9)
-    }
-}
+        };
+};
 
 class AttackShip extends Ship {
     constructor() {
-        super("Attack Ship", 45, 12)
-    }
-}
+        super("Attack Ship", 45, 12, "attack-ship");
+    };
+};
 
 class DefenceShip extends Ship {
     constructor() {
-        super("Defence Ship", 80, 10)
-    }
-}
+    super("Defence Ship", 80, 10, "defence-ship");
+    };
+};
+
+class MotherShip extends Ship {
+    constructor() {
+        super("Mothership", 100, 9, "mothership");
+        };
+};
 
 const shipContainer = document.querySelector(".ship-container");
 
+let alienShipArray = [];
+
 const populateAlienShipArray = () => {
 
-    let alienShipArray = [];
+    alienShipArray = [];
 
     const numOfMotherShips = 1
     const numOfAttackShips = 8
     const numOfDefenceShips = 5
-    const totalAlienShips = numOfAttackShips + numOfAttackShips + numOfDefenceShips;
+    const totalAlienShips = numOfMotherShips + numOfAttackShips + numOfDefenceShips;
 
     for (let i = 0; i < totalAlienShips; i++) {
 
@@ -61,69 +58,54 @@ const populateAlienShipArray = () => {
 
         if (i < numOfMotherShips) {
             newMotherShip = new MotherShip();
-            alienShipArray.push(newMotherShip);
+        alienShipArray.push(newMotherShip);
         } else if (i <= numOfAttackShips) {
-            newAttackShip = new AttackShip();
+            newAttackShip = new AttackShip(); 
             alienShipArray.push(newAttackShip);
-        } else if (i < totalAlienShips) {
+        } else if (i <= totalAlienShips) {
             newDefenceShip = new DefenceShip();
             alienShipArray.push(newDefenceShip);
-        }
-    }
-}
-
-// how would this work with appendChild etc?? need a seperate reset function?
-// const populateHTML = () => {
-//     shipContainer.removeChild()
-
-//     alienShipArray.forEach(ship => {
-//         const alienShipDiv = document.createElement("div")
-//         const alienShipName = document.createElement("p")
-//         const alienShipHP = document.createElement("p")
-
-//         shipContainer.appendChild(alienShipDiv)
-//         alienShipDiv.appendChild(alienShipName)
-//         alienShipDiv.appendChild(alienShipHP)
-//     })
-// }
-
-
-const populateHTML = () => {
-    shipContainer.innerHTML = "";
-
-    alienShipArray.forEach(ship => {
-        shipContainer.innerHTML += `<div><p>${ship.shipName}</p><p>${ship.hitPoints}</p>`
-    });
-};
-
-const attackRandomShip = () => {
-    const randomShipIndex = [Math.floor(Math.random() * alienShipArray.length)]
-    const randomShip = alienShipArray[randomShipIndex];
-    randomShip.attackShip();
-    if (randomShip.hitPoints <= 0 && randomShip.shipName === "Mothership") {
-        alienShipArray = [];
-        alert("The Aliens have been defeated! You are VICTORIOUS!")
-        populateHTML()
-    } else if (randomShip.hitPoints <= 0) {
-        alienShipArray, splice(randomShipIndex, 1);
+        };
     };
 };
 
-const onClickDamageShip = () => {
-    populateHTML();
-    attackRandomShip(alienShipArray)
-}
+const populateTheHTML = () => {
+    shipContainer.innerHTML = "";
+    alienShipArray.forEach(ship => {
+        shipContainer.innerHTML += `<div class="${ship.className}"><p>${ship.shipName}: ${ship.hitPoints}</p></div>`;
+    });
+};
+
+const damageRandomShip = () => {
+    const randomShipIndex = [Math.floor(Math.random() * alienShipArray.length)]
+    const randomShip = alienShipArray[randomShipIndex];
+    randomShip.damageShip();
+    if (randomShip.hitPoints <= 0 && randomShip.shipName === "Mothership") {
+        alienShipArray = [];
+        populateTheHTML();
+        alert("You defeated the aliens! You are VICTORIOUS!");
+    } else if (randomShip.hitPoints <= 0) {
+        alienShipArray.splice(randomShipIndex, 1);
+    };
+};
+
+const doDamageOnClick = () => {
+    populateTheHTML();
+    damageRandomShip(alienShipArray);
+};
 
 const startGame = () => {
- populateAlienShipArray()
- populateHTML()
-}
+    populateAlienShipArray();
+    populateTheHTML();
+};
 
+fireButton.addEventListener("click", () => {
+    doDamageOnClick()
+})
 
-// const ship1 = new Ship ("Attack Ship 1",10,4)
-
-// ship1.attackDamage();
-// ship1.attackDamage();
-// ship1.attackDamage();
+newGameButton.addEventListener("click", () => {
+    startGame()
+    console.log(alienShipArray)
+})
 
 
